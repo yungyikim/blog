aed41c3beadbe1048d74db3863d3c60eb7110bf9
kim yungyi <kimyungyi@kimui-MacBook-Pro.local>
2016-05-20 12:54:27 +0900

    featured 추가

--- js/index.js
+++ js/index.js
@@ -27,6 +27,40 @@ Page.prototype = {
             self.bind();
         });
 
+        $('.container').slick({
+            dots: false,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+        });
+
     },
     bind: function() {
         var self = this;
