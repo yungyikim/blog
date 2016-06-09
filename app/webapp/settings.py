b050caf09b06d53c377366b418a1855c038b6556
kim yungyi <kimyungyi@kimui-MacBook-Pro.local>
2016-06-10 01:47:27 +0900

    소셜 로그인 추가

--- app/webapp/settings.py
+++ app/webapp/settings.py
@@ -91,7 +91,7 @@ SOCIAL_AUTH_PIPELINE = (
     'api.social.create_user',
     'social.pipeline.social_auth.associate_user',
     'social.pipeline.social_auth.load_extra_data',
-    'social.pipeline.user.user_details'
+    'social.pipeline.user.user_details',
 )
 
 
